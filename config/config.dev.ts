2d2367c0327c96b21f8776a6d89f41216676a7bc
qui <tranphanchau10@gmail.com>
2022-01-04 13:50:08 +0700

    update

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   define: {
-    SERVER_API: 'http://localhost:9001',
+    SERVER_API: 'https://free-rice-api.herokuapp.com',
     SOCKET_URL: 'http://localhost:3334',
     HOST_NAME: 'http://localhost:8000',
     KEY_GOOGLE_MAP: 'AIzaSyDCm32QP-RshEWhMbzvFAgR-87qMlnFQEw',
